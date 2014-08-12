1a7b0d2afa93883daa21581018fa4549cb95e051
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-12 13:33:35 -0400

    Exercise 2 answers

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+
+#Question 1
+puts 55 * 0.12
+
+#Question 2
+3.to_s
+my_string = "I am"
+puts my_string + "3"
+
+#Question 3
+a = 45628
+b =  7839
+puts "The result of a and b together is #{a+b}."
+
+#Question 4
+True
+
+
