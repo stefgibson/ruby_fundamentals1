efe9a836fb5a7775262fa7318faf8cbdbdbb9860
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-12 14:58:28 -0400

    Completed exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,14 +1,15 @@
 one_to_100 = (1..100)
 one_to_100.each do |i|
-if i % 3 == 0
+if i % 3 == 0 && i % 5 == 0
+  puts "BitMaker"
+elsif i % 3 == 0
   puts "Bit"
+elsif i % 5 == 0
+  puts "Maker"
 else
   puts i
 end
 end
 
 
-# elsif i % 5 == 0
-#   puts "Maker"
-# else i % 3 == 0 && i % 5 == 0
-#   puts "Bitmaker"
+
