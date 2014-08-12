a9ad7769fc2c79d8579967d3c7e34094bb335887
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-12 14:51:32 -0400

    The Bit potion of my code has been added

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+one_to_100 = (1..100)
+one_to_100.each do |i|
+if i % 3 == 0
+  puts "Bit"
+else
+  puts i
+end
+end
+
+
+# elsif i % 5 == 0
+#   puts "Maker"
+# else i % 3 == 0 && i % 5 == 0
+#   puts "Bitmaker"
